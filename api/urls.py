f605e5b944f9080e093be704d1ed32d66e8c1644
Browwniass <95659478+Browwniass@users.noreply.github.com>
2024-09-17 12:47:03 +0300

    refactor: delete unnecessary reg root

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,5 @@
 from api.spectacular.urls import urlpatterns as doc_urls
 from django.urls import path, include
-from users.urls import urlpatterns as users_urls
 from tasks.urls import urlpatterns as tasks_urls
 
 
@@ -12,5 +11,4 @@ urlpatterns = [
 ]
 
 urlpatterns += doc_urls
-urlpatterns += users_urls
 urlpatterns += tasks_urls
